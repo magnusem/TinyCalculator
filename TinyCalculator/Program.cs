4203ca17486080a35e5bf76729cb65f962c13177
Mag <Mag@DESKTOP-FOAK3KV>
2017-06-09 16:08:13 +0200

    Add project files.

---
+++ TinyCalculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirstInput;
+            int SecondInput;
+            int Result = 0;
+            string Method;
+
+            Console.WriteLine("// TinyCalculator v0.1 //");
+            Console.WriteLine("Please enter your first number");
+            FirstInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now enter second number");
+            SecondInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose your calculating method (*, +, -, /)");
+            Method = Console.ReadLine();
+
+            if (Method == "*")
+            {
+                Result = FirstInput * SecondInput;
+            }
+            else if (Method == "+")
+            {
+                Result = FirstInput + SecondInput;
+            }
+            else if (Method == "-")
+            {
+                Result = FirstInput - FirstInput;
+            }
+            else if (Method == "%")
+            {
+                Result = FirstInput / SecondInput;
+            }
+
+            Console.WriteLine("Result: " + Result);
+
+            Console.WriteLine("Press any key to exit the application");
+            Console.ReadKey();
+        }
+    }
+}
